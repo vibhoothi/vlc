3dfc0ada0f15968e4469f37db8d90f119a70d9ac
Vibhoothi <vibhoothiiaanand@gmail.com>
2018-09-11 21:52:05 +0530

    libraryView: Make new Cocoa class for playQueue
    Make new class with XIB file for having the playQueue and build them

---
+++ modules/gui/macosx/VLCPlayQueue.h
@@ -0,0 +1,12 @@
+//
+//  VLCPlayQueue.h
+//  Pseudo-VLC
+//
+//  Created by Vibhoothi on 11/09/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VLCPlayQueue : NSCollectionViewItem
+
+@end
---
+++ modules/gui/macosx/VLCPlayQueue.m
@@ -0,0 +1,21 @@
+//
+//  VLCPlayQueue.m
+//  Pseudo-VLC
+//
+//  Created by Vibhoothi on 11/09/18.
+//
+
+#import "VLCPlayQueue.h"
+
+@interface VLCPlayQueue ()
+
+@end
+
+@implementation VLCPlayQueue
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
